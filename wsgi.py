c8888c9edcab81bc56bf9ae314dd3b387974f4b5
qusai438 <qsay634@gmail.com>
2025-08-19 06:12:55 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,14 @@
-from app import create_app
+import os
+from flask import Flask
+from app.core.routes import core_bp
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.register_blueprint(core_bp)
+    return app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port)
